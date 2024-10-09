2e47b379fc087f93f18e5f7aebeba7ea0f393eeb
maelstrom <maelstrom@maelstrom.dev>
2024-10-09 17:50:20 +0200

    Somethin's cookin here!

---
+++ assets/shaders/skybox.vs
@@ -0,0 +1,20 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
+
+out vec3 vPos;
+out vec3 vNormal;
+out vec2 vTexCoords;
+
+uniform mat4 model;
+uniform mat3 normalMatrix;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    gl_Position = projection * vec4(vec3(view * vec4(aPos, 0.0)), 1.0);
+    vPos = vec3(vec4(aPos, 1.0));
+    vNormal = aNormal;
+}
--- src/rendering/renderer.cpp
+++ src/rendering/renderer.cpp
@@ -17,6 +17,7 @@
 #include "renderer.h"
 
 Shader *shader = NULL;
+Shader *skyboxShader = NULL;
 extern Camera camera;
 extern std::vector<Part> parts;
 
@@ -29,11 +30,10 @@ void renderInit(GLFWwindow* window) {
 
     // Compile shader
     shader = new Shader("assets/shaders/phong.vs", "assets/shaders/phong.fs");
+    skyboxShader = new Shader("assets/shaders/skybox.vs", "assets/shaders/skybox.fs");
 }
 
-void render(GLFWwindow* window) {
-    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void renderParts() {
 
     // Use shader
     shader->use();
@@ -85,4 +85,60 @@ void render(GLFWwindow* window) {
         CUBE_MESH->bind();
         glDrawArrays(GL_TRIANGLES, 0, 36);
     }
+}
+
+void renderSkyBox() {
+    skyboxShader->use();
+
+    // glm::mat4 projection = glm::perspective(glm::radians(45.f), (float)1200 / (float)900, 0.1f, 100.0f);
+    // glm::mat4 view = camera.getLookAt();
+    
+    // shader->set("projection", projection);
+    // shader->set("view", view);
+    // shader->set("material", Material {
+    //     // .ambient = glm::vec3(1.0f, 0.5f, 0.31f),
+    //     .diffuse = glm::vec3(1.0f, 0.5f, 0.31f),
+    //     .specular = glm::vec3(0.5f, 0.5f, 0.5f),
+    //     .shininess = 32.0f,
+    // });
+
+    // shader->set("viewPos", camera.cameraPos);
+
+// view/projection transformations
+    glm::mat4 projection = glm::perspective(glm::radians(45.f), (float)1200 / (float)900, 0.1f, 100.0f);
+    glm::mat4 view = camera.getLookAt();
+    skyboxShader->set("projection", projection);
+    skyboxShader->set("view", view);
+    skyboxShader->set("material", Material {
+        // .ambient = glm::vec3(1.0f, 0.5f, 0.31f),
+        .diffuse = glm::vec3(1.0f, 0.5f, 0.31f),
+        .specular = glm::vec3(0.5f, 0.5f, 0.5f),
+        .shininess = 32.0f,
+    });
+    skyboxShader->set("sunLight", DirLight {
+        .direction = glm::vec3(-0.2f, -1.0f, -0.3f),
+        .ambient = glm::vec3(0.2f, 0.2f, 0.2f),
+        .diffuse = glm::vec3(0.5f, 0.5f, 0.5f),
+        .specular = glm::vec3(1.0f, 1.0f, 1.0f),
+    });
+    skyboxShader->set("numPointLights", 0);
+
+    glm::mat4 model = glm::mat4(1.0f);
+    // model = glm::translate(model, part.position);
+    // model = model * glm::mat4_cast(part.rotation);
+    // model = glm::scale(model, part.scale);
+    shader->set("model", model);
+    glm::mat3 normalMatrix = glm::mat3(glm::transpose(glm::inverse(model)));
+    shader->set("normalMatrix", normalMatrix);
+
+    CUBE_MESH->bind();
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+void render(GLFWwindow* window) {
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    renderSkyBox();
+    // renderParts();
 }
