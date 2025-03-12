bfaab69daa3a64fbd6d3e2a724050bb4c05ed50e
maelstrom <maelstrom@maelstrom.dev>
2025-03-12 01:22:20 +0100

    chore: add warning message when running run.sh without a parameter

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+if [ $# -eq 0 ] || ([ "$1" != "editor" ] && [ "$1" != "client" ]); then echo "Argument missing, must be 'client' or 'editor'"; exit; fi
+
 [ "$2" = "-debug" ] && CMAKE_OPTS=-DCMAKE_BUILD_TYPE=Debug
 [ "$2" = "-release" ] && CMAKE_OPTS=-DCMAKE_BUILD_TYPE=Release
 [ "$2" = "-reldbg" ] && CMAKE_OPTS=-DCMAKE_BUILD_TYPE=RelWithDebInfo
