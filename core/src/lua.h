3cb5d34ced6f57c19b524b959ffb3101f9fedc2f
maelstrom <maelstrom@maelstrom.dev>
2025-05-02 23:12:42 +0200

    chore: ported lua to vcpkg

--- core/src/lua.h
+++ core/src/lua.h
@@ -1,7 +1,7 @@
 #pragma once
 extern "C" {
-    #include <luajit-2.1/luajit.h>
-    #include <luajit-2.1/lauxlib.h>
-    #include <luajit-2.1/lualib.h>
-    #include <luajit-2.1/lua.h>
+    #include <luajit.h>
+    #include <lauxlib.h>
+    #include <lualib.h>
+    #include <lua.h>
 }
