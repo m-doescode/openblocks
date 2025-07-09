092c3c748dbeca21a377ff224fd4a44667b815fa
maelstrom <maelstrom@maelstrom.dev>
2025-07-09 02:04:25 +0200

    feat(rendering): ttf font rendering

--- assets/shaders/font.fs
+++ assets/shaders/font.fs
@@ -1,22 +1,14 @@
+// https://learnopengl.com/In-Practice/Text-Rendering
+
 #version 330 core
+in vec2 TexCoords;
+out vec4 color;
 
-out vec4 FragColor;
-in vec3 vPos;
-in vec2 vTexCoord;
+uniform sampler2D text;
+uniform vec3 textColor;
 
-uniform sampler2D fontTex;
-uniform int charIndex;
-
-// Main
-
-void main() {
-    int x = (charIndex-32) % 16;
-    int y = (charIndex-32) / 16;
-
-    float fx = float(x) / 16;
-    float fy = float(y) / 8;
-
-   vec4 color = texture(fontTex, vec2(fx, fy) + vTexCoord * vec2(1.f/32, 1.f/16));
-   FragColor = vec3(color) == vec3(0, 0, 0) ? vec4(0, 0, 0, 0) : color;
-//    FragColor = color;
-}
+void main()
+{    
+    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
+    color = vec4(textColor, 1.0) * sampled;
+}  
--- assets/shaders/font.vs
+++ assets/shaders/font.vs
@@ -1,12 +1,13 @@
-#version 330 core
-in vec3 aPos;
-in vec2 aTexCoord;
+// https://learnopengl.com/In-Practice/Text-Rendering
 
-out vec3 vPos;
-out vec2 vTexCoord;
+#version 330 core
+layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 tex>
+out vec2 TexCoords;
+
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
-    vPos = aPos;
-    vTexCoord = aTexCoord;
+    gl_Position = projection * vec4(vertex.xy, 0.0, 1.0);
+    TexCoords = vertex.zw;
 }
