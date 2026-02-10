1e96c3c32d5108bc34c3dbefefa62e0ff52e9f51
maelstrom <maelstrom@maelstrom.dev>
2026-02-10 19:33:30 +0100

    wip(objmodelv2): properties

--- core/src/objectmodel/type.h
+++ core/src/objectmodel/type.h
@@ -1,15 +1,12 @@
 #pragma once
 
+#include <map>
+#include <memory>
 #include <string>
+#include <functional>
+#include "datatypes/variant.h"
 
-struct InstanceType2 {
-    std::string className;
-};
-
-template <typename T /* TODO: Add SFINAE */ >
-const InstanceType2 make_instance_type(std::string name) {
-    return { name };
-}
+class Instance2; // TEMPORARY
 
 #define INSTANCE_HEADER_SOURCE  \
 public: \
@@ -17,4 +14,60 @@ public: \
         static const InstanceType2 type = __buildType(); \
         return type; \
     } \
+\
+    inline const InstanceType2& GetType() override { \
+        return Type();\
+    } \
 private:
+
+using PropertyGetter = std::function<Variant(std::shared_ptr<Instance2>)>;
+using PropertySetter = std::function<void(std::shared_ptr<Instance2>, Variant)>;
+
+struct InstanceProperty {
+    std::string name;
+
+    PropertyGetter getter;
+    PropertySetter setter;
+};
+
+struct InstanceType2 {
+    std::string className;
+    std::map<std::string, InstanceProperty> properties;
+};
+
+inline void __instance_type_add_member(InstanceType2& type, InstanceProperty property) {
+    // TODO: Add error checks here
+
+    type.properties[property.name] = property;
+}
+
+template <typename T, typename ...Args /* TODO: Add SFINAE */ >
+const InstanceType2 make_instance_type(std::string name, Args... args) {
+    InstanceType2 type;
+    type.className = name;
+
+    (__instance_type_add_member(type, args), ...);
+
+    return type;
+}
+
+template <typename T, typename C>
+InstanceProperty def_property(std::string name, T C::* ref) {
+    return {
+        name,
+        [ref](std::shared_ptr<Instance2> instance) {
+            auto obj = std::dynamic_pointer_cast<C>(instance);
+            return obj.get()->*ref;
+        },
+        [ref](std::shared_ptr<Instance2> instance, Variant value) {
+            auto obj = std::dynamic_pointer_cast<C>(instance);
+            obj.get()->*ref = value.get<T>();
+        }
+    };
+}
+
+// TEMPORARY
+
+class Instance2 {
+    virtual const InstanceType2& GetType() = 0;
+};
--- tests/src/objectmodelv2/members.cpp
+++ tests/src/objectmodelv2/members.cpp
@@ -1,13 +1,23 @@
 #include <catch2/catch_test_macros.hpp>
-#include "objects/base/instance.h"
+#include <memory>
 #include "objectmodel/type.h"
+#include "datatypes/primitives.h"
 
-class TestInstance : public Instance {
+class TestInstance : public Instance2 {
 public:
+    int x;
+    std::string y;
 
+    TestInstance() {
+    }
 private:
     static const InstanceType2 __buildType() {
-        return make_instance_type<TestInstance>("TestInstance");
+        return make_instance_type<TestInstance>(
+            "TestInstance",
+            
+            def_property("x", &TestInstance::x),
+            def_property("y", &TestInstance::y)
+        );
     }
 
     INSTANCE_HEADER_SOURCE
@@ -16,5 +26,30 @@ private:
 TEST_CASE("Introspection") {
     SECTION ("Name") {
         REQUIRE(TestInstance::Type().className == "TestInstance");
+    }
+
+    SECTION ("Properties") {
+        auto testInstance = std::make_shared<TestInstance>();
+        testInstance->x = 123;
+        testInstance->y = "Hello, world!";
+
+        auto type = testInstance->GetType();
+        REQUIRE(type.properties.size() == 2);
+        
+        auto xProp = type.properties["x"];
+        REQUIRE(xProp.name == "x");
+        REQUIRE(xProp.getter(testInstance).GetType() == &INT_TYPE);
+        REQUIRE(xProp.getter(testInstance).get<int>() == 123);
+        
+        auto yProp = type.properties["y"];
+        REQUIRE(yProp.name == "y");
+        REQUIRE(yProp.getter(testInstance).GetType() == &STRING_TYPE);
+        REQUIRE(yProp.getter(testInstance).get<std::string>() == "Hello, world!");
+
+        xProp.setter(testInstance, 456);
+        yProp.setter(testInstance, "Foo, bar!");
+
+        REQUIRE(testInstance->x == 456);
+        REQUIRE(testInstance->y == "Foo, bar!");
     }
 }
