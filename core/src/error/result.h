55b03542746ac37e9ece4e9c94962048d8158ae6
maelstrom <maelstrom@maelstrom.dev>
2026-02-18 15:47:54 +0100

    feat: get specific error and generic error in result

--- core/src/error/result.h
+++ core/src/error/result.h
@@ -3,6 +3,7 @@
 #include "error/error.h"
 #include "logger.h"
 #include "panic.h"
+#include <memory>
 #include <optional>
 #include <string>
 #include <type_traits>
@@ -15,6 +16,8 @@ namespace detail {
 template <typename T_Result, typename ...T_Errors>
 class [[nodiscard]] result {
     static_assert(std::conjunction_v<std::is_base_of<Error, T_Errors>...>, "result<T_Errors...> requires T_Errors to derive from Error");
+
+    using T_VariantType = std::variant<T_Errors...>;
 
     struct error_state {
         std::variant<T_Errors...> error;
@@ -48,6 +51,20 @@ public:
 
     std::optional<T_Result> success() { return isSuccess() ? std::make_optional(std::get<success_state>(value).success) : std::nullopt; }
     std::optional<std::variant<T_Errors...>> error() { return isError() ? std::make_optional(std::get<error_state>(value).error) : std::nullopt; }
+
+    template <typename T_Error> std::optional<T_Error> error() {
+        static_assert(std::is_constructible_v<T_VariantType, T_Error>, "T_Error passed to error<T_Error>() must be one of T_Errors...");
+        if (isSuccess()) return std::nullopt;
+        auto&& inner_error = std::get<error_state>(value).error;
+        if (!std::holds_alternative<T_Error>(inner_error)) return std::nullopt;
+        return std::get<T_Error>(inner_error);
+    }
+
+    std::optional<std::unique_ptr<Error>> generic_error() {
+        if (isSuccess()) return std::nullopt;
+        Error* errPtr = std::visit([] (auto&& err) { return &err; }, std::get<error_state>(value).error);
+        return std::make_unique<Error>(*errPtr);
+    }
 
     void logError(Logger::LogLevel logLevel = Logger::LogLevel::ERROR) {
         if (isSuccess()) return;
