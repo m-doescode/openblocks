f943622c868657d9f47edadf03ae20a215bd60b4
Fesiug <publicfesiug@outlook.com>
2026-02-22 21:27:02 -0500

    Message and Hints can have their Text changed from properties

--- core/src/objects/message.cpp
+++ core/src/objects/message.cpp
@@ -4,7 +4,12 @@
 INSTANCE_IMPL(Message)
 
 InstanceType Message::__buildType() {
-    return make_instance_type<Message>("Message", set_explorer_icon("message"));
+    return make_instance_type<Message>("Message",
+        set_explorer_icon("message"),
+
+        set_property_category("appearance"),
+        def_property("Text", &Message::text)
+    );
 }
 
 Message::~Message() = default;
